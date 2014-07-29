9c20863635051149f2c68596f50141d7885e6297
Stephanie Claudino Daffara <ste.daff@gmail.com>
2014-07-28 20:38:28 -0700

    added mobile screen

--- server/public/javascript/map.js
+++ server/public/javascript/map.js
@@ -1,91 +1,92 @@
 $(document).ready(function(){
 
+  function displayCircles(id, startingSizeDataPoint, startingColorPercentage, animate, updateDelay, transitionDelay) {
+     /* circle visualization settings that use a data point between 0-400 to determine x, y and radius */
+     var circleRadius = d3.scale.linear().domain([0, 400]).range(["5", 125]);
 
-      function displayCircles(id, startingSizeDataPoint, startingColorPercentage, animate, updateDelay, transitionDelay) {
-        /* circle visualization settings that use a data point between 0-400 to determine x, y and radius */
-        var circleRadius = d3.scale.linear().domain([0, 400]).range(["5", 125]);
+     // to leave circle in middle
+     var circleYaxis = d3.scale.linear().domain([0, 400]).range(["50%", "50%"]);
+     var circleXaxis = d3.scale.linear().domain([0, 400]).range(["50%", "50%"]);
 
-        // to leave circle in middle
-        var circleYaxis = d3.scale.linear().domain([0, 400]).range(["50%", "50%"]);
-        var circleXaxis = d3.scale.linear().domain([0, 400]).range(["50%", "50%"]);
+     /* "stop light" color range for circle from green through yellow, orange and red using 0-100 as the range */
+     var circleColorRange = d3.scale.linear().domain([0,33,67,100]).range(["green", "#FFCC00", "#FF9900", "red"]);
 
-        /* "stop light" color range for circle from green through yellow, orange and red using 0-100 as the range */
-        var circleColorRange = d3.scale.linear().domain([0,33,67,100]).range(["green", "#FFCC00", "#FF9900", "red"]);
+     // create an SVG element inside the #graph div that fills 100% of the div
+     var vis = d3.select(id).append("svg:svg").attr("width", "100%").attr("height", "100%");
+     /* add a circle -- we don't use the data point, as we will set it manually to animate it, so just passing in [1] */
+     var circle = vis.selectAll("circle").data([1]).enter().append("svg:circle");
 
-        // create an SVG element inside the #graph div that fills 100% of the div
-        var vis = d3.select(id).append("svg:svg").attr("width", "100%").attr("height", "100%");
-        /* add a circle -- we don't use the data point, as we will set it manually to animate it, so just passing in [1] */
-        var circle = vis.selectAll("circle").data([1]).enter().append("svg:circle");
+     /* setup the initial styling and sizing of the circle */
+     circle
+       .style("fill", circleColorRange(startingColorPercentage))
+       .attr("cx", circleXaxis(startingSizeDataPoint))
+       .attr("cy", circleYaxis(startingSizeDataPoint))
+       .attr("r", circleRadius(startingSizeDataPoint));
 
-        /* setup the initial styling and sizing of the circle */
-        circle
-          .style("fill", circleColorRange(startingColorPercentage))
-          .attr("cx", circleXaxis(startingSizeDataPoint))
-          .attr("cy", circleYaxis(startingSizeDataPoint))
-          .attr("r", circleRadius(startingSizeDataPoint));
+     function redraw(dataValue, colorPercentage, stage) {
+       // update the circle
+       d3.selectAll('#'+stage).selectAll("circle")
+         .transition()
+         .duration(transitionDelay)
+         .attr("cy", circleYaxis(dataValue))
+         .attr("cx", circleXaxis(dataValue))
+         .attr("r", circleRadius(dataValue))
+         .style("fill", circleColorRange(colorPercentage));
+     }
 
-        function redraw(dataValue, colorPercentage, stage) {
-          // update the circle
-          console.log(stage);
-          d3.selectAll('#'+stage).selectAll("circle")
-            .transition()
-            .duration(transitionDelay)
-            .attr("cy", circleYaxis(dataValue))
-            .attr("cx", circleXaxis(dataValue))
-            .attr("r", circleRadius(dataValue))
-            .style("fill", circleColorRange(colorPercentage));
-        }
+     // set maxSize, whatever is the biggest value from dataset
+     var maxSize = 4.000;
 
-        // set maxSize, whatever is the biggest value from dataset
-        var maxSize = 8.000;
+     //input: data size; output: circle size
+     function dataToSize(data) {
+       return ((data*200)/maxSize)+20;
+     }
 
-        //input: data size; output: circle size
-        function dataToSize(data) {
-          return ((data*220)/maxSize)+20;
-        }
+     //input: data size; output: color number (100 = red .. 0 = green)
+     function sizeToColor(size) {
+       colorNumber = ((size*100)/maxSize);
+       return colorNumber;
+     }
 
-        //input: data size; output: color number (100 = red .. 0 = green)
-        function sizeToColor(size) {
-          colorNumber = ((size*100)/maxSize);
-          return colorNumber;
-        }
+     function danceData() {
+       $.ajax('/api',{success:function(data, textStatus) {
+         d=JSON.parse(data).Rock;
+         e=JSON.parse(data)['Hip-hop'];
+         if(isNaN(d)) {
+           d = 0;
+         }
+         if(isNaN(e)) {
+           e = 0;
+         }
+         getSizeAndColor(d, e);
+         console.log(data);
+       }});
+     }
 
-        ////////////////////////////////////////////// BOTH STAGES
+     var getSizeAndColor = function(rockIntensity, hiphopIntensity){
 
-        function danceData() {
-          $.ajax('/api',{success:function(data, textStatus) {
-            d=JSON.parse(data).Rock;
-            e=JSON.parse(data).Hip-hop;
-            if(!isNaN(d) && !isNaN(e)) {
-              getSizeAndColor(d, e);
-            }
-          }});
-        }
+       var sizeRock = Math.floor(dataToSize(rockIntensity));
+       var colorRock = sizeToColor(rockIntensity);
 
-        var getSizeAndColor = function(rockIntensity, hiphopIntensity){
+       var sizeHipHop = Math.floor(dataToSize(hiphopIntensity));
+       var colorHipHop = sizeToColor(hiphopIntensity);
 
-          var sizeRock = Math.floor(dataToSize(rockIntensity));
-          var colorRock = sizeToColor(rockIntensity);
+       redraw(sizeRock, colorRock, "stage1");
+       redraw(sizeHipHop, colorHipHop, "stage2");
 
-          var sizeHipHop = Math.floor(dataToSize(hiphopIntensity));
-          var colorHipHop = sizeToColor(hiphopIntesnity);
+     };
 
-          redraw(sizeRock, colorRock, "stage1");
-          redraw(SizeHipHop, colorHipHop, "stage2");
-          console.log("size one = " + sizeOne + ", color one = "+ colorOne);
-
-        };
-
-        if(animate) {
-          setInterval(function() {
-            danceData();
-          }, updateDelay);
-        }
-
-
-      // id, startingSizeDataPoint, startingColorPercentage, animate, updateDelay, transitionDelay
-        displayCircles("#stage1", 40, 5, true, 400, 400);
-        displayCircles("#stage2", 40, 5, true, 400, 400);
+     if(animate) {
+       setInterval(function() {
+         danceData();
+       }, updateDelay);
+     }
 
   }
+
+  // id, startingSizeDataPoint, startingColorPercentage, animate, updateDelay, transitionDelay
+  displayCircles("#stage1", 0, 0, true, 400, 400);
+  displayCircles("#stage2", 0, 0, true, 400, 400);
+
+
 });
